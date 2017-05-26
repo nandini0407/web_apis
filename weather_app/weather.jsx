23e90023474f60a970719ed398ffb09206a23f2c
nandini0407 <nandini0407@gmail.com>
2017-05-26 12:00:14 -0700

    make api to call to request weather

--- weather_app/weather.jsx
+++ weather_app/weather.jsx
@@ -2,10 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import store from './store';
 import App from './app';
+import { getWeather } from './weather_actions';
 
 document.addEventListener('DOMContentLoaded', () => {
   const root = document.getElementById("root");
   ReactDOM.render(<App store={ store }/>, root);
 
   window.store = store;
+  window.getWeather = getWeather;
 });
--- weather_app/weather_reducer.js
+++ weather_app/weather_reducer.js
@@ -1,6 +1,13 @@
+import { RECEIVE_WEATHER } from './weather_actions';
 
 const weatherReducer = function(state = {}, action) {
-
+  Object.freeze(state);
+  switch(action.type) {
+    case RECEIVE_WEATHER:
+      return action.weather;
+    default:
+      return state;
+  }
 };
 
 export default weatherReducer;
